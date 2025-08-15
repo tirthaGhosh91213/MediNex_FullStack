7f029c94171164e28e469184eb5bea5111cbc4da
Tirtha Ghosh <ghoshtirtha1234@gmail.com>
2025-08-15 21:02:36 +0530

    setup  the project

---
+++ frontend_mediNex/src/Appoinments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Appoinments = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Appoinments
---
+++ frontend_mediNex/src/pages/MyAppinment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyAppinment = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyAppinment
